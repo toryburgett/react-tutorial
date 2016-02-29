ac65f42db30c85f47c38b51a14ca0cadf1c03762
Tory Burgett <vdburgett@gmail.com>
2016-02-29 16:56:27 -0500

    lesson 23, NotFound now in ES6

--- scripts/components/NotFound.js
+++ scripts/components/NotFound.js
@@ -3,10 +3,10 @@
 */
 import React from 'react';
 
-var NotFound = React.createClass({
-  render: function(){
+class NotFound extends React.Component {
+  render(){
     return <h1>Not Found!</h1>
   }
-})
+}
 
 export default NotFound;
