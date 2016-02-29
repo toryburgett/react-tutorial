17255c35ca6365777a481a92697422103f4b51a5
Tory Burgett <vdburgett@gmail.com>
2016-02-29 13:41:56 -0500

    Lesson 17, bidirectional data binding with react catalyst

--- scripts/main.js
+++ scripts/main.js
@@ -13,10 +13,17 @@ var h = require("./helpers.js")
 var Rebase = require('re-base');
 var base = Rebase.createClass("https://catchy-catchy.firebaseio.com/");
 
+/* React Catalyst, bi directional data flow */
+var Catalyst = require('react-catalyst');
+
+
+
+
 /*
   App
 */
 var App = React.createClass({
+  mixins: [Catalyst.LinkedStateMixin],
   getInitialState : function(){
     return{
       fishes: {},
@@ -67,7 +74,7 @@ var App = React.createClass({
           </ul>
         </div>
         <Order fishes={this.state.fishes} order={this.state.order}/>
-        <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} fishes={this.state.fishes} linkState={this.linkState}/>
       </div>
     )
   }
@@ -215,10 +222,27 @@ var Order = React.createClass({
   <Inventory />
 */
 var Inventory = React.createClass({
+  renderInventory : function(key){
+    var linkState = this.props.linkState;
+    return(
+      <div className="fish-edit" key={key}>
+        <input type="text" valueLink={linkState('fishes.'+key+'.name')}/>
+        <input type="text" valueLink={linkState('fishes.'+key+'.price')}/>
+        <select valueLink={linkState('fishes.'+key+'.status')}>
+          <option value="unavailable">SOLD OUT!</option>
+          <option value="available">FRESH!</option>
+        </select>
+        <textarea valueLink={linkState('fishes.'+key+'.desc')}></textarea>
+        <input type="text" valueLink={linkState('fishes.'+key+'.image')}/>
+        <button>REMOVE FISH</button>
+      </div>
+    )
+  },
   render : function(){
     return(
       <div>
         <h2>Inventory</h2>
+        {Object.keys(this.props.fishes).map(this.renderInventory)}
         <AddFishForm {...this.props} />
         <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
       </div>
