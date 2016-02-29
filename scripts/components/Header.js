980c5c37e1251d3f2cf8cfb2f4e1387fe41520c2
Tory Burgett <vdburgett@gmail.com>
2016-02-29 17:23:36 -0500

    lesson 26, ES6 Proptypes in Header

--- scripts/components/Header.js
+++ scripts/components/Header.js
@@ -4,9 +4,11 @@
 */
 
 import React from 'react';
+import autobind from 'autobind-decorator';
 
-var Header = React.createClass({
-  render : function(){
+@autobind
+class Header extends React.Component{
+  render(){
     return(
       <header className="top">
         <h1>Catch
@@ -18,10 +20,11 @@ var Header = React.createClass({
         <h3 className="tagline"><span>{this.props.tagline}</span></h3>
       </header>
     )
-  },
-  propTypes : {
-    tagline : React.PropTypes.string.isRequired
   }
-});
+}
+
+Header.propTypes = {
+  tagline : React.PropTypes.string.isRequired
+}
 
 export default Header;
