ed75a3bae70e23b52419efc635cff4aa73425976
Tory Burgett <vdburgett@gmail.com>
2016-02-29 17:12:05 -0500

    lesson 24, mixins with ES6, also jumped ahead to binding with ES6

--- scripts/components/StorePicker.js
+++ scripts/components/StorePicker.js
@@ -5,18 +5,20 @@
 import React from 'react';
 import { History } from 'react-router';
 import h from '../helpers';
+import reactMixin from 'react-mixin';
+import autobind from 'autobind-decorator';
 
-var StorePicker = React.createClass({
-  mixins : [History],
-  goToStore : function(event){
+@autobind
+class StorePicker extends React.Component {
+
+  goToStore(event){
     event.preventDefault();
     var storeId = this.refs.storeId.value;
-    console.log(storeId);
-
     this.history.pushState(null, '/store/' + storeId);
 
-  },
-  render : function(){
+  }
+
+  render(){
     var name = "Tory";
     return(
       <form className="store-selector" onSubmit={this.goToStore}>
@@ -27,6 +29,8 @@ var StorePicker = React.createClass({
       </form>
     )
   }
-});
+}
+
+reactMixin.onClass(StorePicker, History);
 
 export default StorePicker;
